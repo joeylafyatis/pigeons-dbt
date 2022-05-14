32f1c0f13329bb3cede1b990955f001802a53406
Joey Lafyatis <joeylafyatis@Joeys-MacBook-Air.local>
2022-05-14 17:26:53 -0400

    more sql files

---
+++ _sql_table/fact_goals.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS fact_goals;
+CREATE TABLE fact_goals (
+    match_date INTEGER NOT NULL
+    , match_year INTEGER NOT NULL
+    , match_month INTEGER NOT NULL
+    , match_day INTEGER NOT NULL
+    , opponent VARCHAR NOT NULL REFERENCES dim_opponent(opponent)
+    , scored_by VARCHAR NOT NULL REFERENCES dim_player(player)
+    , scored_at INTEGER NOT NULL
+    , scored_at_extra_time INTEGER
+    , assisted_by VARCHAR REFERENCES dim_player(player)
+    , is_penalty_kick BOOLEAN NOT NULL
+    , PRIMARY KEY (match_date, opponent)
+);
--- _sql_table/fact_lineups.sql
+++ _sql_table/fact_lineups.sql
@@ -5,11 +5,12 @@ CREATE TABLE fact_lineups (
     , match_month INTEGER NOT NULL
     , match_day INTEGER NOT NULL
     , opponent VARCHAR NOT NULL REFERENCES dim_opponent(opponent)
-    , player_name VARCHAR NOT NULL REFERENCES dim_player(player)
+    , player VARCHAR NOT NULL REFERENCES dim_player(player)
     , is_starting_xi BOOLEAN NOT NULL
     , substitution_for VARCHAR REFERENCES dim_player(player)
     , substitution_off_at INTEGER
     , substitution_on_at INTEGER
     , yellow_card_at INTEGER
     , red_card_at INTEGER
+    , PRIMARY KEY (match_date, opponent)
 );
