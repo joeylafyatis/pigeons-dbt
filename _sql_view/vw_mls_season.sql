acee79b45dbf48c3fdb44493daba2a65e703dfce
Joey Lafyatis <joeylafyatis@Joeys-MacBook-Air.local>
2022-05-27 13:40:11 -0400

    mls season stats

---
+++ _sql_view/vw_mls_season.sql
@@ -0,0 +1,41 @@
+DROP VIEW IF EXISTS vw_mls_season;
+CREATE VIEW vw_mls_season AS 
+WITH mls_regular_season AS (
+    SELECT
+        match_date 
+        , match_year
+        , match_month 
+        , match_day
+        , ROW_NUMBER() OVER(PARTITION BY match_year ORDER BY match_date ASC) AS mls_matchday
+        , opponent
+        , manager
+        , stadium
+        , attendance
+        , referee
+        , is_home_match
+        , nycfc_goals
+        , opponent_goals
+        , result
+        , CASE result
+            WHEN 'W' THEN 3
+            WHEN 'D' THEN 1
+            WHEN 'L' THEN 0
+            END AS points
+        , SUM(
+            CASE result
+                WHEN 'W' THEN 3
+                WHEN 'D' THEN 1
+                WHEN 'L' THEN 0
+                END
+        ) OVER (
+            PARTITION BY match_year
+            ORDER BY match_date ASC
+            ROWS UNBOUNDED PRECEDING
+        ) AS cumulative_points
+    FROM fact_matches 
+    WHERE competition = 'MLS Regular Season'
+)
+SELECT *
+    , ROUND(cumulative_points / CAST(mls_matchday AS FLOAT), 4) AS cumulative_ppg
+FROM mls_regular_season
+;
---
+++ _sql_view/vw_stats_by_mls_season.sql
@@ -0,0 +1,76 @@
+DROP VIEW IF EXISTS vw_stats_by_mls_season;
+CREATE VIEW vw_stats_by_mls_season AS 
+WITH mls_by_season AS (
+    SELECT 
+        match_year AS mls_season
+        , COUNT(1) AS matches_played
+        , SUM(CASE WHEN result = 'W' THEN 1 END) AS wins
+        , SUM(CASE WHEN result = 'D' THEN 1 END) AS draws
+        , SUM(CASE WHEN result = 'L' THEN 1 END) AS losses
+        , SUM(points) AS points
+        , SUM(nycfc_goals) AS goals_for
+        , SUM(opponent_goals) AS goals_against
+        , SUM(CASE WHEN opponent_goals = 0 THEN 1 END) AS clean_sheats
+        , SUM(CASE WHEN nycfc_goals = 0 THEN 1 END) AS clean_sheats_against
+        -- HOME
+        , COUNT(CASE WHEN is_home_match THEN 1 END) AS matches_played_home
+        , SUM(CASE WHEN is_home_match AND result = 'W' THEN 1 END) AS wins_home
+        , SUM(CASE WHEN is_home_match AND result = 'D' THEN 1 END) AS draws_home
+        , SUM(CASE WHEN is_home_match AND result = 'L' THEN 1 END) AS losses_home
+        , SUM(CASE WHEN is_home_match THEN points END) AS points_home
+        , SUM(CASE WHEN is_home_match THEN nycfc_goals END) AS goals_for_home
+        , SUM(CASE WHEN is_home_match THEN opponent_goals END) AS goals_against_home
+        , SUM(CASE WHEN is_home_match AND opponent_goals = 0 THEN 1 END) AS clean_sheats_home
+        , SUM(CASE WHEN is_home_match AND nycfc_goals = 0 THEN 1 END) AS clean_sheats_against_home
+        -- AWAY
+        , COUNT(CASE WHEN NOT is_home_match THEN 1 END) AS matches_played_away
+        , SUM(CASE WHEN NOT is_home_match AND result = 'W' THEN 1 END) AS wins_away
+        , SUM(CASE WHEN NOT is_home_match AND result = 'D' THEN 1 END) AS draws_away
+        , SUM(CASE WHEN NOT is_home_match AND result = 'L' THEN 1 END) AS losses_away
+        , SUM(CASE WHEN NOT is_home_match THEN points END) AS points_away
+        , SUM(CASE WHEN NOT is_home_match THEN nycfc_goals END) AS goals_for_away
+        , SUM(CASE WHEN NOT is_home_match THEN opponent_goals END) AS goals_against_away
+        , SUM(CASE WHEN NOT is_home_match AND opponent_goals = 0 THEN 1 END) AS clean_sheats_away
+        , SUM(CASE WHEN NOT is_home_match AND nycfc_goals = 0 THEN 1 END) AS clean_sheats_against_away
+    FROM vw_mls_season
+    GROUP BY 1
+)
+SELECT         
+    mls_season
+    , matches_played
+    , wins
+    , draws
+    , losses
+    , points
+    , ROUND(points / CAST(matches_played AS FLOAT), 2) AS points_per_game
+    , goals_for
+    , goals_against
+    , goals_for - goals_against AS goal_diff
+    , clean_sheats
+    , clean_sheats_against
+    -- HOME
+    , matches_played_home
+    , wins_home
+    , IFNULL(draws_home, 0) AS draws_home
+    , losses_home
+    , points_home
+    , ROUND(points_home / CAST(matches_played_home AS FLOAT), 2) AS points_per_game_home
+    , goals_for_home
+    , goals_against_home
+    , goals_for_home - goals_against_home AS goal_diff_home
+    , clean_sheats_home
+    , clean_sheats_against_home
+    -- AWAY
+    , matches_played_away
+    , wins_away
+    , draws_away
+    , losses_away
+    , points_away
+    , ROUND(points_away / CAST(matches_played_away AS FLOAT), 2) AS points_per_game_away
+    , goals_for_away
+    , goals_against_away
+    , goals_for_away - goals_against_away AS goal_diff_away
+    , clean_sheats_away
+    , clean_sheats_against_away
+FROM mls_by_season
+;
